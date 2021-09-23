d9fadf236389d0f629147e1f930c16bb615a981a
Lex <gtagaev96@mail.ru>
2021-09-23 14:58:37 +0700

    start

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp (
       routes: MainNavigation().routes,
       initialRoute: MainNavigation().initialRoute,
     );
